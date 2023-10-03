5bf44d43fc92aaf125f08587f779bccbba807db5
ayadav16 <ayadav7@binghamton.edu>
2023-10-03 17:42:02 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Apoorv Yadav on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
